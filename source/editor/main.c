4c449075352794d24a47ef5ea37db51999eb4c2d
dwrrehman <dwrrehman@gmail.com>
2020-05-12 11:17:23 -0700

    add the new editor code, i forgot to push it.

--- source/editor/main.c
+++ source/editor/main.c
@@ -5,14 +5,7 @@
 //  Created by Daniel Rehman on 2004245.
 //  Copyright © 2020 Daniel Rehman. All rights reserved.
 //
-#include <stdbool.h>
-#include <stdio.h>
-#include <math.h>
-#include <time.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <termios.h>
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -26,6 +19,11 @@
 #include <termios.h>
 #include <errno.h>
 #include <ctype.h>
+
+/// Parameters:
+
+static size_t wrap_width = 60;
+static size_t tab_width = 8;
 
 typedef char* unicode;
 
@@ -48,6 +46,25 @@ struct line {
     bool continued;
 };
 
+enum key_bindings {
+    edit_key = 'e',         hard_edit_key = 'E',
+    select_key = 's',
+    
+    up_key = 'o',           down_key = 'i',
+    left_key = 'j',         right_key = ';',
+    jump_key = 'k',         find_key = 'l',
+
+    cut_key = 'd',          paste_key = 'a',
+    
+    redo_key = 'r',         undo_key = 'u',
+    rename_key = 'W',       save_key = 'w',
+    force_quit_key = 'Q',   quit_key = 'q',
+
+    option_key = 'p',       function_key = 'f',
+    status_bar_key = '.',   help_key = '?',
+    cursor_key = '|',
+};
+
 const char
     *save_cursor = "\033[s",
     *restore_cursor = "\033[u",
@@ -59,14 +76,27 @@ const char
     *save_screen = "\033[?1049h",
     *restore_screen = "\033[?1049l";
 
-struct termios terminal = {0};
+const char
+    *left_exit = "wf",
+    *right_exit = "oj",
+    *edit_exit = "wq",
 
-static const size_t wrap_width = 100;
-static const size_t tab_width = 4;
+    *jump_top = "ko",
+    *jump_bottom = "km",
+    *jump_begin = "kj",
+    *jump_end = "kl";
+
+
+struct termios terminal = {0};
 
 static inline void debug_panic() {
     printf("internal error! aborting...\n");
     abort();
+}
+
+static inline void debug_error() {
+    printf("internal error! aborting...\n");
+    sleep(1);
 }
 
 static inline void restore_terminal() {
@@ -95,93 +125,78 @@ static inline char read_byte_from_stdin() {
     if (n < 0) {
         printf("n < 0 : ");
         perror("read(STDIN_FILENO, &c, 1) syscall");
-        debug_panic();
+        debug_error();
     } else if (n == 0) {
         printf("n == 0 : ");
         perror("read(STDIN_FILENO, &c, 1) syscall");
-        debug_panic();
+        debug_error();
     }
     return c;
 }
 
-static inline bool is(unicode u, char c) {
-    return u && !strncmp(u, &c, 1);
-}
+static inline bool is(unicode u, char c) { return u && !strncmp(u, &c, 1); }
+static inline bool bigraph(const char* seq, unicode c0, unicode c1) { return is(c0, seq[1]) && is(c1, seq[0]); }
 
 static inline unicode read_unicode() {
-    unicode bytes = NULL;
-    size_t byte_count = 0;
-    char c;
-    do {
-        c = read_byte_from_stdin();
-        bytes = realloc(bytes, sizeof(char) * (byte_count + 1));
-        bytes[byte_count++] = c;
-    } while (c > 127);
-    bytes = realloc(bytes, sizeof(char) * (byte_count + 1));
-    bytes[byte_count++] = '\0';
+    unicode bytes = 0; size_t total = 0;
+    unsigned char c = read_byte_from_stdin();
+    bytes = realloc(bytes, sizeof(char) * (total + 1));
+    bytes[total++] = c;
+    size_t trailing_bytes = 0;
+    if ((c >> 3) == 30) trailing_bytes = 3;
+    else if ((c >> 4) == 14) trailing_bytes = 2;
+    else if ((c >> 5) == 6) trailing_bytes = 1;
+    for (size_t i = 0; i < trailing_bytes; i++) {
+        bytes = realloc(bytes, sizeof(char) * (total + 1));
+        bytes[total++] = read_byte_from_stdin();
+    }
+    bytes = realloc(bytes, sizeof(char) * (total + 1));
+    bytes[total++] = '\0';
     return bytes;
 }
 
 void insert(unicode c, size_t at, unicode** text, size_t* length) {
-    if (at > *length) debug_panic();
+    if (at > *length) { debug_error(); return; }
     *text = realloc(*text, sizeof(unicode) * (*length + 1));
     memmove(*text + at + 1, *text + at, sizeof(unicode) * (*length - at));
     ++*length; (*text)[at] = c;
 }
 
 static inline void delete(size_t at, unicode** text, size_t* length) {
-    if (at > *length) debug_panic();
+    if (at > *length) { debug_error(); return; }
     if (!at || !*length) return;
     memmove((*text) + at - 1, (*text) + at, sizeof(unicode) * (*length - at));
     *text = realloc(*text, sizeof(unicode) * (--*length));
 }
 
-
-static inline void display(struct line* lines, size_t line_count, struct location origin, struct location cursor, struct winsize window) {
-    
+static inline void display
+ (struct line* lines,
+  size_t line_count,
+  struct location origin,
+  struct location cursor,
+  struct winsize window) {
     size_t text_length = 0;
-    char screen_text[window.ws_col * window.ws_row];
-    memset(screen_text, 0, sizeof screen_text);
-    struct location pointer_loc = {1,1};
-    
+    char screen[window.ws_col * window.ws_row];
+    memset(screen, 0, sizeof screen);
+    struct location screen_cursor = {1,1};
     for (size_t line = origin.line; line < fmin(origin.line + window.ws_row - 1, line_count); line++) {
         for (size_t column = origin.column; column < fmin(origin.column + window.ws_col - 1, lines[line].length); column++) {
             unicode g = lines[line].line[column];
             if (line < cursor.line || (line == cursor.line && column < cursor.column)) {
-                if (is(g, '\t')) { pointer_loc.column += tab_width; }
-                else pointer_loc.column++;
+                if (is(g, '\t')) screen_cursor.column += tab_width;
+                else screen_cursor.column++; //+= strlen(g);
             }
-            if (is(g, '\t')) for (size_t i = 0; i < tab_width; i++) screen_text[text_length++] = ' ';
-            else for (size_t i = 0; i < strlen(g); i++) screen_text[text_length++] = g[i];
-        }
-        if (line < cursor.line) { pointer_loc.line++; pointer_loc.column = 1; }
-        screen_text[text_length++] = '\n';
+            if (is(g, '\t')) for (size_t i = 0; i < tab_width; i++) screen[text_length++] = ' ';
+            else for (size_t i = 0; i < strlen(g); i++) screen[text_length++] = g[i];
+        } if (line < cursor.line) {
+            screen_cursor.line++;
+            screen_cursor.column = 1;
+        } screen[text_length++] = '\n';
     }
     printf("%s", clear_screen);
-    printf("%s", screen_text);
-    printf(set_cursor, pointer_loc.line, pointer_loc.column);
+    printf("%s", screen);
+    printf(set_cursor, screen_cursor.line, screen_cursor.column);
 }
-
-
-
-//printf("%s", before);
-
-
-
-
-
-//        if (0 && line < cursor.line)
-
-//            before[before_count++] = '\n';
-
-//        else
-
-
-
-
-
-
-
 
 static inline struct line* generate_line_view(unicode* text, size_t text_length, size_t* count) {
     *count = 0;
@@ -198,17 +213,12 @@ static inline struct line* generate_line_view(unicode* text, size_t text_length,
     }
     
     for (size_t i = 0; i < text_length; i++) {
-        
-        
-    
+
         if (is(text[i], '\n')) {
             lines[*count - 1].continued = false;
             length = 0;
-        } else if (is(text[i], '\t')) {
-            length += tab_width;
-            lines[*count - 1].length++;
         } else {
-            length++;
+            if (is(text[i], '\t')) length += tab_width; else length++;
             lines[*count - 1].length++;
         }
         
@@ -251,13 +261,11 @@ static inline void move_left
   bool user) {
     if (cursor->column) {
         cursor->column--;
-        if (screen->column) screen->column--;
-        else if (origin->column) origin->column--;
+        if (screen->column) screen->column--; else if (origin->column) origin->column--;
     } else if (cursor->line) {
-        cursor->column = lines[cursor->line - 1].length - lines[cursor->line].continued;
         cursor->line--;
-        if (screen->line) screen->line--;
-        else if (origin->line) origin->line--;
+        cursor->column = lines[cursor->line].length - lines[cursor->line].continued;
+        if (screen->line) screen->line--; else if (origin->line) origin->line--;
         if (cursor->column > window.ws_col - 1) {
             screen->column = window.ws_col - 1;
             origin->column = cursor->column - screen->column;
@@ -280,13 +288,10 @@ static inline void move_right
   bool user) {
     if (cursor->column < lines[cursor->line].length) {
         cursor->column++;
-        if (screen->column < window.ws_col - 1) screen->column++;
-        else origin->column++;
+        if (screen->column < window.ws_col - 1) screen->column++; else origin->column++;
     } else if (cursor->line < line_count - 1) {
-        cursor->line++;
-        if (screen->line < window.ws_row - 2) screen->line++;
-        else origin->line++;
-        cursor->column = lines[cursor->line].continued;
+        cursor->column = lines[cursor->line++].continued;
+        if (screen->line < window.ws_row - 2) screen->line++; else origin->line++;
         if (cursor->column > window.ws_col - 1) {
             screen->column = window.ws_col - 1;
             origin->column = cursor->column - screen->column;
@@ -294,8 +299,7 @@ static inline void move_right
             screen->column = cursor->column;
             origin->column = 0;
         }
-    }
-    if (user) *desired = *cursor;
+    } if (user) *desired = *cursor;
 }
 
 
@@ -310,17 +314,14 @@ static inline void move_up
     
     if (!cursor->line) {
         *screen = *cursor = *origin = (struct location){0, 0};
-        *point = 0;
-        return;
+        *point = 0; return;
     }
-    
     const size_t column_target = fmin(lines[cursor->line - 1].length, desired->column);
     const size_t line_target = cursor->line - 1;
     while (cursor->column > column_target || cursor->line > line_target) {
         move_left(cursor, origin, screen, window, lines, desired, false);
-        (*point)++;
+        (*point)--;
     }
-    
     if (cursor->column > window.ws_col - 1) {
         screen->column = window.ws_col - 1;
         origin->column = cursor->column - screen->column;
@@ -340,24 +341,20 @@ static inline void move_down
   size_t line_count,
   size_t length,
   struct location* desired) {
-     
+    
     if (cursor->line >= line_count - 1) {
-        while (*point < length - 1) {
+        while (*point < length) {
             move_right(cursor, origin, screen, window, lines, line_count, length, desired, false);
             (*point)++;
-        }
-        return;
+        } return;
     }
-     
     const size_t column_target = fmin(lines[cursor->line + 1].length, desired->column);
     const size_t line_target = cursor->line + 1;
-    
     while (cursor->column < column_target || cursor->line < line_target) {
         move_right(cursor, origin, screen, window, lines, line_count, length, desired, false);
         (*point)++;
     }
-    
-    if (lines[cursor->line].continued && !column_target) {
+    if (cursor->line && lines[cursor->line - 1].continued && !column_target) {
         move_left(cursor, origin, screen, window, lines, desired, false);
         (*point)--;
     }
@@ -365,15 +362,30 @@ static inline void move_down
 
 
 
+static void jump
+ (unicode c0,
+  struct location *cursor,
+  struct location *origin,
+  struct location *screen,
+  struct location *desired,
+  struct winsize window,
+  size_t* point,
+  size_t length,
+  struct line* lines,
+  size_t line_count
+  ) {
+    unicode c1 = read_unicode();
+    if (bigraph(jump_top, c0, c1)) { *screen = *cursor = *origin = (struct location){0, 0}; *point = 0; }
+    else if (bigraph(jump_bottom, c0, c1)) while (*point < length - 1) move_down(cursor, origin, screen, window, point, lines, line_count, length, desired);
+    else if (bigraph(jump_begin, c0, c1)) while (cursor->column) move_left(cursor, origin, screen, window, lines, desired, true);
+    else if (bigraph(jump_end, c0, c1)) while (cursor->column < lines[cursor->line].length) move_right(cursor, origin, screen, window, lines, line_count, length, desired, true);
+}
+
 
 int main(const int argc, const char** argv) {
-        
-    printf("%s", save_screen);
-    configure_terminal();
-    
+    enum editor_mode mode = edit_mode;
     size_t length = 0, at = 0, line_count = 0;
     unicode *text = 0, c = 0, p = 0;
-    
     struct location
         origin = {0,0},
         cursor = {0,0},
@@ -381,72 +393,63 @@ int main(const int argc, const char** argv) {
         desired = {0,0};
     
     struct winsize window;
+    printf("%s", save_screen);
+    configure_terminal();
+    
     struct line* lines = generate_line_view(text, length, &line_count);
     
-    while (!is(c, 'q')) {
+    while (mode != quit) {
         
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &window);
         display(lines, line_count, origin, cursor, window);
-//        debug_lines(lines, line_count);
-        fflush(stdout);
-        c = read_unicode();
+        fflush(stdout); c = read_unicode();
         
-        if (is(c, 127)) {
-            if (at && length) {
+        if (mode == command_mode) {
+            if (is(c, edit_key)) mode = edit_mode;
+            else if (is(c, up_key)) move_up(&cursor, &origin, &screen, window, &at, lines, &desired);
+            else if (is(c, down_key)) move_down(&cursor, &origin, &screen, window, &at, lines, line_count, length, &desired);
+            else if (is(c, left_key) && at < length) { at++; move_right(&cursor, &origin, &screen, window, lines, line_count, length, &desired, true); }
+            else if (is(c, right_key) && at) { at--; move_left(&cursor, &origin, &screen, window, lines, &desired, true); }
+            else if (is(c, jump_key)) jump(c, &cursor, &origin, &screen, &desired, window, &at, length, lines, line_count);
+                                    
+        } else {
+            if (bigraph(left_exit, c, p) ||
+                bigraph(right_exit, c, p) ||
+                bigraph(edit_exit, c, p)) {
                 delete(at--, &text, &length);
                 move_left(&cursor, &origin, &screen, window, lines, &desired, true);
                 free(lines);
                 lines = generate_line_view(text, length, &line_count);
-            }
-        } else if (is(c, 27)) {
-            c = read_unicode();
-            if (is(c, '[')) {
-                c = read_unicode();
-                if (is(c, 'A')) move_up(&cursor, &origin, &screen, window, &at, lines, &desired);
-                else if (is(c, 'B')) move_down(&cursor, &origin, &screen, window, &at, lines, line_count, length, &desired);
-                if (is(c, 'C') && at < length) {
-                    move_right(&cursor, &origin, &screen, window, lines, line_count, length, &desired, true);
-                    at++;
-                }
-                else if (is(c, 'D') && at) {
+                mode = command_mode;
+                if (bigraph(edit_exit, c, p)) { mode = quit; } /// THEN QUIT.
+            
+            } else if (is(c, 127)) {
+                if (at && length) {
+                    delete(at--, &text, &length);
                     move_left(&cursor, &origin, &screen, window, lines, &desired, true);
-                    at--;
+                    free(lines);
+                    lines = generate_line_view(text, length, &line_count);
                 }
+            } else if (is(c, 27)) {
+                unicode c = read_unicode();
+                if (is(c, '[')) {
+                    unicode c = read_unicode();
+                         if (is(c, 'A')) move_up(&cursor, &origin, &screen, window, &at, lines, &desired);
+                    else if (is(c, 'B')) move_down(&cursor, &origin, &screen, window, &at, lines, line_count, length, &desired);
+                    else if (is(c, 'C') && at < length) { at++; move_right(&cursor, &origin, &screen, window, lines, line_count, length, &desired, true); }
+                    else if (is(c, 'D') && at) { at--; move_left(&cursor, &origin, &screen, window, lines, &desired, true); }
+                } else if (is(c, 27)) mode = command_mode;
+                
+            } else {
+                insert(c, at++, &text, &length);
+                free(lines);
+                lines = generate_line_view(text, length, &line_count);
+                move_right(&cursor, &origin, &screen, window, lines, line_count, length, &desired, true);
             }
-        } else {
-            insert(c, at++, &text, &length);
-            free(lines);
-            lines = generate_line_view(text, length, &line_count);
-            move_right(&cursor, &origin, &screen, window, lines, line_count, length, &desired, true);
         }
         p = c;
     }
-        
+    
     restore_terminal();
     printf("%s", restore_screen);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//static inline size_t count_tabs(struct location cursor, struct location origin, struct line* lines) {
-//    size_t count = 0;
-//    for (size_t i = origin.column; i < cursor.column; i++)
-//        if (is(lines[cursor.line].line[i], '\t')) count++;
-//    return count;
-//}
-
-
-        
-//       + (tab_width - 1) * count_tabs(cursor, origin, lines)
-
