c49eedf44a0d8c03311acc8ce2b897163ef73265
Daniel Rehman <dwrrehman@gmail.com>
2023-05-28 20:05:20 -0700

    push the current simpler editor code. its really coming along now, i still have to simplify the user api a ton though.

--- even_simpler_version/e.c
+++ even_simpler_version/e.c
@@ -6,14 +6,17 @@
 #include <stdbool.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <unistd.h>
 static void handler(int __attribute__((unused))_) {}
 int main(int argc, const char** argv) {
 	char* text = NULL, * input = NULL;
-	size_t capacity = 0, count = 0, cursor = 0, anchor = 0, saved = 1, mode = 1, max = 0;
+	size_t capacity = 0, count = 0, cursor = 0, anchor = 0, saved = 1, mode = 1, max = 128;
 	char filename[4096] = {0};
 	struct sigaction action = {.sa_handler = handler};
 	sigaction(SIGINT, &action, NULL);
+
 	if (argc < 2) goto loop;
+
 	FILE* file = fopen(argv[1], "r");
 	if (not file) { perror("fopen"); goto done; }
 	fseek(file, 0, SEEK_END);
@@ -21,80 +24,116 @@ int main(int argc, const char** argv) {
         fseek(file, 0, SEEK_SET); fread(text, 1, count, file);
 	fclose(file); mode = 2; 
 	strlcpy(filename, argv[1], sizeof filename);
-	printf("%s: %lu\n", filename, count);
+	printf("%lu\n", count);
+	cursor = 0;
 
 loop:;	ssize_t r = getline(&input, &capacity, stdin);
 	if (r <= 0) goto save_exit;
 	size_t length = (size_t) r;
 	if (mode == 1) {
+		if (length >= 2 and input[length - 2] == '`') { 
+			length -= 2; mode = 2; puts("."); 
+		}
 		text = realloc(text, count + length);
-		memcpy(text + cursor + length, text + cursor, count - cursor);
+		memmove(text + cursor + length, text + cursor, count - cursor);
 		for (size_t i = 0; i < length; i++) text[cursor + i] = input[i];
 		count += length; cursor += length; saved = 0;
-		if (length >= 2 and input[length - 2] == '`') { printf("%lu\n", cursor); mode = 2; }
-
+		
 	} else if (mode == 2) {
 		input[--length] = 0;
 		if (not strcmp(input, "")) puts("[empty]");
 		else if (not strcmp(input, "clear")) printf("\033[2J\033[H");
 		else if (not strcmp(input, "quit")) { if (saved) mode = 0; else puts("modified"); }
+
 		else if (not strcmp(input, "discard_and_quit")) mode = 0;
 		else if (not strcmp(input, "insert")) mode = 1;
 		else if (not strcmp(input, "drop")) anchor = cursor;
 		else if (not strcmp(input, "back")) cursor = anchor;
-		else if (not strcmp(input, "print_contents")) { fwrite(text, count, 1, stdout); puts(""); } // debug
-		else if (not strcmp(input, "print")) { fwrite(text + cursor, count - cursor, 1, stdout); puts(""); }
+		else if (not strcmp(input, "top")) cursor = 0;
+		else if (not strcmp(input, "bottom")) cursor = count - 1;
+		else if (not strcmp(input, "right")) cursor++;
+		else if (not strcmp(input, "left")) cursor--;
+		else if (not strcmp(input, "filename")) puts(filename);
+		else if (input[0] == '.') max = (size_t) atoi(input + 1);
+
 		else if (not strcmp(input, "count")) printf("%lu %lu %lu\n", count, anchor, cursor);
+		else if (not strcmp(input, "print_contents")) { fwrite(text, count, 1, stdout); }
+		else if (not strcmp(input, "print_after")) { fwrite(text + cursor, count - cursor, 1, stdout);  }
+
+		else if (not strcmp(input, "print")) { 
+			fwrite(text + cursor, count - cursor < max ? count - cursor : max, 1, stdout); 
+			puts("");
+		}
+
+		else if (not strcmp(input, "print_anchor")) { 
+			fwrite(text + anchor, count - anchor < max ? count - anchor : max, 1, stdout); 
+			puts("");
+		}
+
+
 
 		else if (not strcmp(input, "cut")) {
 			if (cursor < anchor) { size_t temp = cursor; cursor = anchor; anchor = temp; }
-			memcpy(text + anchor, text + cursor, count - cursor);
+			memmove(text + anchor, text + cursor, count - cursor);
 			count -= cursor - anchor;
 			text = realloc(text, count);
 			cursor = anchor;
 		}
 
 		else if (input[0] == '/') {
+			if (cursor >= count) { puts("/ error"); goto loop; }
 
-			const char* offset = strstr(text, input + 1);
+			const char* offset = strstr(text + cursor + 1, input + 1);
 			if (not offset) printf("absent %s\n", input + 1);
 			else {
-				cursor = (size_t) (offset - text);
+				cursor += (size_t) (offset - (text + cursor));
 				printf("%lu\n", cursor);
 			}
-
-		} else if (input[0] == '\\') {
-
-			const char* offset = strstr(text, input + 1);
-			if (not offset) printf("absent %s\n", input + 1);
-			else {
-				cursor = (size_t) (offset - text);
-				printf("%lu\n", cursor);
-			}
-
-		} else if (input[0] == '.') { 
-
-			max = (size_t) atoi(input + 1);
-			fwrite(text + cursor, count - cursor < max ? count - cursor : max, 1, stdout); 
-			puts("");
 		}
 
-
-		else if (not strcmp(input, "chead")) { 
-			fwrite(text + cursor, count - cursor < max ? count - cursor : max, 1, stdout); 
-			puts("");
+		else if (input[0] == '\\') {
+			puts("backwards is currently unimplemented");
 		}
 
-		else if (not strcmp(input, "ahead")) { 
-			fwrite(text + anchor, count - anchor < max ? count - anchor : max, 1, stdout); 
+		else if (not strcmp(input, "up")) { 
+			size_t save = cursor;
+			if (cursor > max) cursor -= max; else cursor = 0;
+			fwrite(text + cursor, save - cursor, 1, stdout); 
 			puts("");
-		}
+		} 
 
-		else if (not strcmp(input, "page")) { 
+		else if (not strcmp(input, "down")) { 
 			size_t total = count - cursor < max ? count - cursor : max;
 			fwrite(text + cursor, total, 1, stdout); 
 			puts("");
 			cursor += total;
+		}
+
+		else if (not strncmp(input, "read ", 5)) { 
+			if (not saved) { puts("modified"); goto loop; }
+			FILE* infile = fopen(input + 5, "r");
+			if (not infile) { perror("fopen"); goto loop; }
+
+			fseek(infile, 0, SEEK_END); free(text); 
+			count = (size_t) ftell(infile); text = malloc(count);
+			fseek(infile, 0, SEEK_SET); fread(text, 1, count, infile);
+			fclose(infile); mode = 2; 
+			strlcpy(filename, input + 5, sizeof filename);
+			printf("%s: %lu\n", filename, count);
+		} 
+
+		else if (not strncmp(input, "name ", 7)) {
+			if (access(input + 7, F_OK) != -1) puts("file exists");
+			else strlcpy(filename, input + 7, sizeof filename);
+		}
+
+		else if (not strcmp(input, "write")) {
+			if (not *filename) { puts("unnamed"); goto loop; }
+			FILE* output_file = fopen(filename, "w");
+			if (not output_file) { perror("fopen"); goto loop; }
+			fwrite(text, count, 1, output_file);
+			fclose(output_file);
+			saved = 1;
 		}
 
 		else printf("unintelligible %s\n", input);
@@ -102,6 +141,44 @@ loop:;	ssize_t r = getline(&input, &capacity, stdin);
 	if (mode) goto loop;
 	save_exit:; done: printf("exiting...\n");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -246,7 +323,7 @@ loop:;	ssize_t r = getdelim(&input, &capacity, mode == 1 ? delimiter : 10, stdin
 	if (mode == 1) {
 		getchar();
 		text = realloc(text, count + length);
-		memcpy(text + cursor + length, text + cursor, count - cursor);
+		memmove(text + cursor + length, text + cursor, count - cursor);
 		for (size_t i = 0; i < length; i++) text[cursor + i] = input[i];
 		count += length; cursor += length; mode = 2; saved = 0;
 
--- even_simpler_version/find_string_testing/c.c
+++ even_simpler_version/find_string_testing/c.c
@@ -2,20 +2,50 @@
 #include <stdio.h>
 #include <string.h>
 
+static char* find(char* text, char* tofind, int count, int length, int cursor) {
+	for (int i = 0, t = 0; i < count;) {
+		if (text[i] == tofind[t]) { 
+			t++; i++; 
+			if (t == length) return text + i - length;
+		} else { t = 0; i++; }
+	}
+	return NULL;
+}
+
+static char* find_reverse(char* text, char* tofind, int count, int length, int cursor) {
+	for (int i = cursor, t = 0; i > 0;) {
+		if (text[i] == tofind[t]) { 
+			t++; i++; 
+			if (t == length) return text + i - length;
+		} else { t = 0; i--; }
+	}
+	return NULL;
+}
+
 int main(void) {
-	const char* text = "TutorialsPoint is a string. cool beans.";
-	const char* tofind = "Point";
-	const char* r = strstr(text, tofind);
-	printf("The substring is: #%s# %ld.\n", r, r - text);
+
+	char* text = strdup("hello this is my bubbles and beans. my favorite bubbles is perhaps the bubble and beans.");
+	int count = (int) strlen(text);
+
+	char* tofind = strdup("bubbles");
+	int length = (int) strlen(tofind);
+
+	int cursor = 0;
+	
+	char* r = find(text + cursor, tofind, count, length);
+	long location = r - text;
+	printf("MINE: The substring is: #%s# %ld.\n", text + location, location);
+
+	r = strstr(text, tofind);
+	location = r - text;
+	printf("THEM: The substring is: #%s# %ld.\n", text + location, location);
 }
 
 
-\
-\const char* offset = strrstr(text, input + 1);
-			if (not offset) printf("absent %s\n", input + 1);
-			else {
-				cursor = (size_t) (offset - text);
-				printf("%lu\n", cursor);
-			}
+
+
+
+
+
 
 
