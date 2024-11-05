874982ecf37b6fcd4b971c558026dc8f42298e36
Daniel Rehman <dwrrehman@gmail.com>
2024-11-04 22:12:56 -0800

    write the basic start of the editor,untested so far

--- simpler_modal_editor/c.c
+++ simpler_modal_editor/c.c
@@ -1,7 +1,7 @@
-#include <stdio.h>   // 202410266.140132: dwrr
-#include <stdlib.h>  
-#include <string.h>  
-#include <iso646.h>  
+#include <stdio.h>   // written 202410266.140132 by dwrr
+#include <stdlib.h>  // 1202411041.210238 an editor that
+#include <string.h>  // supposed to be a simpler version of
+#include <iso646.h>  // the modal editor that we wrote.
 #include <unistd.h>  
 #include <fcntl.h>
 #include <termios.h>
@@ -21,9 +21,134 @@
 
 typedef uint64_t nat;
 
+struct action {
+	nat parent;
+	nat pre;
+	nat post;
+	nat choice;
+	int64_t length;
+	char* string;
+};
+
+#define max_screen_size  1 << 20
+
+//static const bool use_qwerty_layout = false; 
+static nat cursor = 0,  count = 0, anchor = 0, origin = 0,
+	cliplength = 0, head = 0, action_count = 0;
+static char* text = NULL, * clipboard = NULL;
+static struct action* actions = NULL;
+static char filename[4096] = {0};
+static volatile struct winsize window = {0};
+static struct termios terminal = {0};
+
+static char remap(char c) {
+	if (c == 13) return 10;
+	return c; 
+}
+
+static void append(
+	const char* string, nat length, 
+	char* screen, nat* screen_length) {
+	if (*screen_length + length >= max_screen_size) return;
+	memcpy(screen, string, length);
+	*screen_length += length;
+}
+
+static void display(void) {
+	char screen[max_screen_size];
+	nat length = 0, column = 0, row = 0;
+
+	append("\033[H", 3, screen, &length);
+	nat i = origin;
+	for (; i < count; i++) {
+		const char c = text[i];
+		if (i == cursor or i == anchor) 
+			append("\033[7m", 4, screen, &length);
+
+		if (c == 10) {
+		print_newline:
+			append("\033[K", 3, screen, &length);
+			if (row < window.ws_row - 1) 
+				append("\n", 1, screen, &length);
+			row++; column = 0;
+			if (row >= window.ws_row - 1) {
+				append("\033[0m", 4, screen, &length);
+				break;
+			}
+
+		} else if (c == 9) {
+			nat amount = 8 - column % 8;
+			column += amount;
+			if (i == cursor or i == anchor) { 
+				append(" \033[0m", 5, screen, &length); 
+				amount--; 
+			}
+			append("        ", amount, screen, &length);
+
+		} else if ((unsigned char) c >= 32) {
+			append(text + i, 1, screen, &length); 
+			column++;
+		} else {
+			const char control_code = c + 'A';
+			append("\033[7mCTL", 4, screen, &length); 
+			append(&control_code, 1, screen, &length);
+			append("\033[0m", 4, screen, &length); 
+			column += 4;
+		}
+		if (column >= window.ws_col - 2) goto print_newline;
+		if (i == cursor or i == anchor) append("\033[0m", 4, screen, &length);
+	}
+	if (i == cursor or i == anchor) append("\033[7m \033[0m", 9, screen, &length);
+	while (row < window.ws_row) {
+		append("\033[K", 3, screen, &length);
+		if (row < window.ws_row - 1) append("\n", 1, screen, &length);
+		row++;
+	}
+	write(1, screen, length);
+}
+
+static void finish_action(struct action node, char* string, int64_t length) {
+	node.choice = 0;
+	node.parent = head;
+	node.post = cursor; 
+	node.string = strndup(string, (nat) length);
+	node.length = length;
+	head = action_count;
+	actions = realloc(actions, sizeof(struct action) * (action_count + 1));
+	actions[action_count++] = node;
+}
 
 
+static void insert(char* string, nat length, bool should_record) {
+	struct action node = { .pre = cursor };
+	text = realloc(text, count + length);
+	memmove(text + cursor + length, text + cursor, count - cursor);
+	memcpy(text + cursor, string, length);
+	count += length; cursor += length;
+	if (should_record) finish_action(node, string, (int64_t) length);
+}
 
+static void delete(nat length, bool should_record) {
+	if (cursor < length) return;
+	struct action node = { .pre = cursor };
+	cursor -= length; count -= length; 
+	char* string = strndup(text + cursor, length);
+	memmove(text + cursor, text + cursor + length, count - cursor);
+	text = realloc(text, count);
+	if (should_record) finish_action(node, string, (int64_t) -length);
+}
+
+static void save(void) {
+
+}
+
+static void window_resized(int _) { if(_){} ioctl(0, TIOCGWINSZ, &window); }
+
+static noreturn void interrupted(int _) {if(_){} 
+	write(1, "\033[?25h", 6);
+	tcsetattr(0, TCSANOW, &terminal);
+	save(); exit(0); 
+}
 
 int main(int argc, const char** argv) {
 	struct sigaction action = {.sa_handler = window_resized}; 
@@ -31,7 +156,9 @@ int main(int argc, const char** argv) {
 	struct sigaction action2 = {.sa_handler = interrupted}; 
 	sigaction(SIGINT, &action2, NULL);
 
-
+	if (argc == 1) goto new;
+	else if (argc == 2) strlcpy(filename, argv[1], sizeof filename);
+	else exit(puts("usage: ./editor [file]"));
 
 	int df = open(filename, O_RDONLY | O_DIRECTORY);
 	if (df >= 0) { close(df); errno = EISDIR; goto read_error; }
@@ -43,7 +170,7 @@ int main(int argc, const char** argv) {
 	read(file, text, count);
 	close(file);
 
-new: 	cursor = 0; anchor = disabled;
+new: 	cursor = 0; anchor = 0;
 	finish_action((struct action) {0}, NULL, (int64_t) 0);
 	tcgetattr(0, &terminal);
 	struct termios terminal_copy = terminal; 
@@ -53,26 +180,64 @@ new: 	cursor = 0; anchor = disabled;
 	tcsetattr(0, TCSANOW, &terminal_copy);
 	write(1, "\033[?25l", 6);
 
-
-
+	nat mode = 0;
 
 loop:	ioctl(0, TIOCGWINSZ, &window);
 	display();
-	char c = 0;
-	ssize_t n = read(0, &c, 1);c = remap(c);
-	if (n < 0) { perror("read"); fflush(stderr); }
-	if (mode == insert_mode) {
 
+	char c = 0;
+	ssize_t n = read(0, &c, 1); 
+	c = remap(c);
+	if (n < 0) { perror("read"); fflush(stderr); }
+
+	if (not mode) {
+		if (c == 27) mode = 1;
+		else if (c == 127) delete(1, 1);
+		else if (c == 9 or c == 10 or 
+			(unsigned char) c >= 32)
+			insert(&c, 1, 1);
 	} else {
 		if (c == 27 or c == ' ') {}
-		else if (c == 't') mode = insert_mode;
-
+		else if (c == 't') mode = 0;
+		else if (c == 'Q') goto done;
+		else if (c ==
 	}
 	goto loop;
+
 done:	write(1, "\033[?25h", 6);
 	tcsetattr(0, TCSANOW, &terminal);
 	save(); exit(0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -179,5 +344,90 @@ else if (c == 127) { if (anchor == disabled) delete(1,1); else delete_selection(
 
 
 
+
+
+
+
+
+/*	nat column = 0, row = 0;
+	for (nat i = origin; i < count; i++) {
+
+		const char c = text[i]; 
+
+		if (i == cursor or i == anchor) printf("\033[7m");
+		if (c == 10) putchar(' ');
+
+		if (c == 9) {
+			const nat amount = 8 - column % 8;
+			for (nat _ = 0; _ < amount; _++) putchar(32);
+			column += amount;
+
+		} else if (c == 10) {
+		print_newline: 
+			puts("");
+			column = 0; row++;
+			if (row >= max_row_count) { 
+				at_bottom = false; 
+				printf("\033[0m"); 
+				break; 
+			}
+
+		} else if (c >= 32) {
+			putchar(c);
+			column++;
+		} else {
+			printf("\033[7mCTL%2hhu\033[0m", c);
+			column += 5;
+		}
+		if (column >= wrap_width) goto print_newline;
+
+		if (i == this->cursor or i == this->anchor) printf("\033[0m");
+	}
+
+	bool at_bottom = true;
+{
+
+	nat column = 0;
+	nat row = 0;
+	for (nat i = this->origin; i < this->length; i++) {
+
+		const char c = this->output[i]; 
+
+		if (i == this->cursor or i == this->anchor) printf("\033[7m");
+		if (c == 10) putchar(' ');
+
+		if (c == 9) {
+			const nat amount = 8 - column % 8;
+			for (nat _ = 0; _ < amount; _++) putchar(32);
+			column += amount;
+
+		} else if (c == 10) {
+		print_newline: 
+			puts("");
+			column = 0; row++;
+			if (row >= max_row_count) { at_bottom = false; printf("\033[0m"); break; }
+
+		} else if (c >= 32) {
+			putchar(c);
+			column++;
+		} else {
+			printf("\033[7mCTL%2hhu\033[0m", c);
+			column += 5;
+		}
+		if (column >= wrap_width) goto print_newline;
+
+		if (i == this->cursor or i == this->anchor) printf("\033[0m");
+	}
+	
+}
+	if (not at_bottom) printf("\033[7m[scrolling back]\033[0m");
+	else printf("\033[7m(head)\033[0m");
+
+	fflush(stdout);
+
+
+
+
+*/
 
 
