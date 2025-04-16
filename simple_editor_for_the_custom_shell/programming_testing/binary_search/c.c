81887b6852cc5880f49c4a144d7887a4625dd6b3
Daniel Rehman <dwrrehman@gmail.com>
2025-04-16 12:54:38 -0700

    add an edit to the manual file.

---
+++ simple_editor_for_the_custom_shell/programming_testing/binary_search/c.c
@@ -0,0 +1,20 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    
+    int index = 0;
+
+    while (1) {
+
+        if (target < array[index]) {
+
+            
+        } else {
+
+		
+
+	}
+
+    }
+
+}
+
+
---
+++ simple_editor_for_the_custom_shell/programming_testing/hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {		// a simple program to test my command based editor.
+	puts("hello world");
+	int array[6] = {4, 2, 6, 5, 7, 6 };
+	const int my_length = 6;
+
+	for (int i = 0; i < my_length; i++) {
+		printf("[%u] = %u\n", i, array[i]);
+	}	
+	exit(0);
+}
+
+
+
+1202410222.224115
+	this is a new line that i am adding the end of the document lol. 
+	but yeah it works okay so far lol.
+
+
